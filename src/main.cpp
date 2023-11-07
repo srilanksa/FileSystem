18f5a9d41deb7592830d9517971fd10ac4633a9b
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-11-06 23:13:18 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+struct Group{
+  bool root;
+}
 
+struct DataBlock{
+  DataBlock *inode;
+  Group g;
+  uint8_t data[512 - sizeof(Group) - sizeof(DataBlock *)];
 
+  void 
 
+}
 
 
 
 int main(){
-
+  
+  DataBlock db[10];
   
 
   return 0;
